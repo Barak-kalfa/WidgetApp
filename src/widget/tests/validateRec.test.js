7db204fb8df58ef55e77fa86bdbebf9b03054ff1
Barak Kalfa <barak.kalfa@gmail.com>
2023-06-16 20:48:54 +0300

    thumb tests

--- src/widget/tests/validateRec.test.js
+++ src/widget/tests/validateRec.test.js
@@ -3,16 +3,20 @@ import TEST_SETTINGS from "./tests-settings/widgetTestsSettings.json";
 import { validateRecommendation } from "../validation/validateRec.js";
 
 describe("testing validateRecommendation()", () => {
+  const validImgURL = "https://www.pakainfo.com/wp-content/uploads/2021/09/image-url-for-testing.jpg";
+  const validURL = "https://www.example.com";
+  const invalidURL = "https://www.example.com/invalid-page";
+
   it("it should return false if one of the fields is null", () => {
     const validRec = {
-      url: "https://abc.com",
+      url: validURL,
       origin: TEST_SETTINGS.typeFilter[0],
-      thumbnail: [{ url: "abc.png" }],
+      thumbnail: [{ url:validImgURL }],
     };
     const inValidRec = {
       url: "",
       origin: TEST_SETTINGS.typeFilter[0],
-      thumbnail: [{ url: "abc.png" }],
+      thumbnail: [{ url: validImgURL }],
     };
     const shouldBeTrue = validateRecommendation(validRec, TEST_SETTINGS);
     const shouldBeFalse = validateRecommendation(inValidRec, TEST_SETTINGS);
@@ -23,14 +27,14 @@ describe("testing validateRecommendation()", () => {
 
   it("should return false if the url is not valid", () => {
     const validRec = {
-      url: "https://www.example.com",
+      url: validURL,
       origin: TEST_SETTINGS.typeFilter[0],
-      thumbnail: [{ url: "abc.png" }],
+      thumbnail: [{ url: validImgURL }],
     };
     const inValidRec = {
-      url: "https://www.example.com/invalid-page",
+      url: invalidURL,
       origin: TEST_SETTINGS.typeFilter[0],
-      thumbnail: [{ url: "abc.png" }],
+      thumbnail: [{ url: validImgURL }],
     };
     const shouldBeTrue = validateRecommendation(validRec, TEST_SETTINGS);
     const shouldBeFalse = validateRecommendation(inValidRec, TEST_SETTINGS);
@@ -40,20 +44,10 @@ describe("testing validateRecommendation()", () => {
   });
 
   it("should return false if the image type is not in list", () => {
-    const imgTypes = ["jpg", "png", "svg", "gif", "jpeg"];
-    imgTypes.forEach((type) => {
-      const validRec = {
-        url: "https://www.abc.com",
-        origin: TEST_SETTINGS.typeFilter[0],
-        thumbnail: [{ url: `abc.${type}` }],
-      };
-      const shouldBeTrue = validateRecommendation(validRec, TEST_SETTINGS);
-      expect(shouldBeTrue).toBeTruthy();
-    });
     const inValidRec = {
-      url: "https://www.example.com",
+      url: validURL,
       origin: TEST_SETTINGS.typeFilter[0],
-      thumbnail: [{ url: "abc.js" }],
+      thumbnail: [{ url: validURL }],
     };
     const shouldBeFalse = validateRecommendation(inValidRec, TEST_SETTINGS);
     expect(shouldBeFalse).toBeFalsy();
